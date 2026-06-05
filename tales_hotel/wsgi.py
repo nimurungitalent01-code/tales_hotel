09e97d66991f43e1037c8375ce4963f13c76e9be
Nimurungi Talent <nimururngitalent01@gmail.com>
2026-06-05 07:25:52 +0300

    Fix wsgi syntax

--- tales_hotel/wsgi.py
+++ tales_hotel/wsgi.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi 
-import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tales_hotel.settings')
 
